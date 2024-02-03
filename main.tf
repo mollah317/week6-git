c89555ea9d0deffcb5c8921ce77085a0465ebf7a
mollah317 <morkdiagne@gamil.com>
2024-02-03 09:47:40 -0500

    lightsail

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "us-east-1"  # Update this with your desired region
+}
+
+resource "aws_lightsail_instance" "server1" {
+  name              = "dev-server"
+  blueprint_id      = "ubuntu_18_04"
+  bundle_id         = "medium_1_0"
+  availability_zone = "us-east-1a"
+  user_data         = <<-EOF
+              #!/bin/bash
+              sudo apt-get update
+              sudo apt-get install -y apache2
+              sudo systemctl start apache2
+              sudo systemctl enable apache2
+              echo '<h1>This is deployed by Mor </h1>' | sudo tee /var/www/html/index.html
+              EOF
+}
